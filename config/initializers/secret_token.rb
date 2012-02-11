de5b205754567976e814ed078b43820439f963c7
miio <info@miio.info>
2012-02-11 21:44:43 +0900

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiki::Application.config.secret_token = '99f772612dcd9ac06669087fd9a04c38aef040928abe27a2b2248ab117538a95372363fe4cd1394b0ebb8e6d4ef981523a043527d089dbfdb7776832a9833a22'
